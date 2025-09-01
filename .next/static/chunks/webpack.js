c4223384e6a51d1565c4de3205b0620ccc80d496
badjackson <68308026+badjackson@users.noreply.github.com>
2025-09-01 07:35:38 +0100

    Fix missing closing brace in GrossePriseEntry

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90580c5664c702ab"; }
+/******/ 		__webpack_require__.h = function() { return "ce589dca1fb78e01"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
